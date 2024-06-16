62d236c269a0dfb32cdf241f66af47d56647b468
pradeep <pradeep@pradeep-Lenovo-V15-G2-ITL-Ua>
2024-06-16 10:40:51 +0530

    edited

--- src/fer/com/Health.java
+++ src/fer/com/Health.java
@@ -3,5 +3,6 @@ package fer.com;
 public class Health {
 public static void main(String[] args) {
 	System.out.println("WE can do this");
+	System.out.println("Be yourself; everyone else is already taken.");
 }
 }
