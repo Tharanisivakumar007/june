6f3eafa672693d223c72244fd01dcedb73ac0594
Tharanisivakumar007 <tharusivsivasara@gmail.com>
2024-06-17 10:19:25 +0530

    I have added a condition.

--- src/fer/com/Health.java
+++ src/fer/com/Health.java
@@ -6,6 +6,12 @@ public static void main(String[] args) {
 	System.out.println("Be yourself; everyone else is already taken.");
 	System.out.println("");
 	System.out.println("main branch");
+	String s="Apple";
+	if (s=="Apple") {
+		System.out.println("It is a fruit");
+	} else {
+  System.out.println("It is not a fruit");
+	}
 	
 }
 }
